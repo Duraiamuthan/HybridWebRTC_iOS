e9d6078e5ebf7d5be7b7cade3859b63488387595
duharikr <h.duraiamuthan@gmail.com>
2016-09-16 19:06:42 +0530

    Bridging header for Objective C WebRTC interface
    Bridging header for Objective C WebRTC interface

---
+++ WebRTCSupport/WebRTCSupport-Bridging-Header.h
@@ -0,0 +1,31 @@
+//
+//  Header.h
+//  WebRTCSupport
+//
+//  Created by Durai on 16/09/16.
+//  Copyright © 2016 Durai Amuthan.H. All rights reserved.
+//
+
+
+#import "RTCAudioTrack.h"
+#import "RTCDataChannel.h"
+#import "RTCEAGLVideoView.h"
+#import "RTCICECandidate.h"
+#import "RTCICEServer.h"
+#import "RTCMediaConstraints.h"
+#import "RTCMediaStream.h"
+#import "RTCMediaStreamTrack.h"
+#import "RTCPair.h"
+#import "RTCPeerConnection.h"
+#import "RTCPeerConnectionDelegate.h"
+#import "RTCStatsDelegate.h"
+#import "RTCPeerConnectionFactory.h"
+#import "RTCSessionDescription.h"
+#import "RTCSessionDescriptionDelegate.h"
+#import "RTCStatsReport.h"
+#import "RTCTypes.h"
+#import "RTCVideoCapturer.h"
+#import "RTCVideoRenderer.h"
+#import "RTCVideoSource.h"
+#import "RTCVideoTrack.h"
+#import "RTCAVFoundationVideoSource.h"
